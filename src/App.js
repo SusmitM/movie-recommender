521afd9a7f097f9e0e54ef27f97955db225ba312
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-01-12 23:56:55 +0530

    button function added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 
 import './App.css';
+import { useState } from "react";
+
+
+const movieDB = {
+  Action :
+  [
+    { Name: "The Dark Knight (2008)", rating: "9/10" },
+    { Name:"The Lord of the Rings: The Return of the King (2003)" , rating:"8.9/10" }
+
+  ],
+  Comedy:
+  [
+    { Name: "The Chaos Class (1975)", rating: "9.3/10" },
+    { Name:"Parasite (2019)", rating:"8.6/10" }
+
+  ],
+
+  SciFi:
+  [
+    { Name: "Inception (2010)", rating: "8.8/10" },
+    { Name:"Spider-Man: No Way Home (2021)" , rating:"8.8/10" }
+
+  ]
+
+};
 
 function App() {
+  const [selectedTopic,setTopic] = useState("Action");
+  function clickHandler(topic){
+      setTopic(topic);
+      console.log(selectedTopic);
+   }
+  
   return (
     <div className="App">
+
+      <div className="Header">
      <h1>Movie Recommender</h1>
+     </div>
+     <div className="btnList">
+       {Object.keys(movieDB).map((topic) =>(<button onClick={ () => clickHandler(topic)}>{topic}</button>))}
+       
+     </div>
     </div>
   );
 }
